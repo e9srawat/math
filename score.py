f3a8f00a6fb742206c09683e35c5b69b707c5217
Shivansh <shivansh.rawat@enine.school>
2024-02-12 13:08:18 +0530

    modified:   score.py

--- score.py
+++ score.py
@@ -1,10 +1,7 @@
 import os
 
 path = sorted(os.listdir())
-cont = 1
 for i in path:
     path = os.path.join(i, "solution.py")
     if "math" in path:
-        print(cont)
-        os.system("pylint " + path)
-        cont+=1
+        os.system("python3 " + path)
